472b1d3abbfc9627b8e3accc86d6f4fee02b0614
605spirit-CPU <cncnncncncncn605@gmail.com>
2026-02-19 10:20:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const changeCursorButton = document.getElementById('customCursor');
+const bodyElement = document.getElementById('bodyElement');
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
     //get the button's element ID from index.html
     const button = document.getElementById('actionButton');
@@ -14,3 +18,8 @@ document.getElementById('logs').addEventListener('click', function() {
 document.getElementById('comments').addEventListener('click', function() {
     window.location.href = 'Comments.html'; // Redirects to comments
   });
+
+changeCursorButton.addEventListener('click', function() {
+    bodyElement.classList.toggle('seal-cursor');
+});
+
